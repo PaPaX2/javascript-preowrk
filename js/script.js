143efa27b05465b3c837014fde600b4960427b4f
Kajetan Terlecki <k.terlecki@wp.pl>
2019-08-06 17:24:30 +0200

    Add js Computer Move

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
 
-let computerMove = `kamień`;
-
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
+printMessage('Mój ruch to: ' + computerMove);
